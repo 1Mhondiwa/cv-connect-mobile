0dcc88ecf726c8f757f9c6777c0e9739024330d3
1Mhondiwa <mhondiwatapiwanashe@gmail.com>
2026-09-01 19:58:05 +0200

    fix nonexistent expo api usage in screen capture and image picker

--- src/screens/associate/ProfileScreen.js
+++ src/screens/associate/ProfileScreen.js
@@ -60,7 +60,7 @@ const AssociateProfileScreen = () => {
 
       // Launch image picker
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaType.Images,
+        mediaTypes: ['images'],
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.8,
